aa24b39c11fe91fd124b93b73ebf41911bfe7afd
Junle <lijunle@gmail.com>
2015-10-18 01:50:13 +0800

    Extract package related logic to package model.

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import * as tokenModel from '../models/token';
+import * as packageModel from '../models/package';
 import { table } from '../services';
 
 const jsonParser = bodyParser.json();
@@ -33,33 +34,13 @@ router.route('/token/:provider/:user/:repo')
 
 router.route('/:provider/:user/:repo')
   .get((req, res) => {
-    const { provider, user, repo } = req.params;
-
-    table.query('package', {
-      limit: 1,
-      filter: { provider, user, repo },
-    })
-    .then(([record]) => res.json(record),
+    packageModel.query(req.params)
+    .then(result => res.json(result),
       error => res.send(error));
   })
   .post(jsonParser, (req, res) => {
-    const { provider, user, repo } = req.params;
-    const branch = req.body.branch;
-    const report = req.body.report;
-    const result = req.body.result;
-    const id = `${provider}:${user}:${repo}-$KEY$-${branch}:${report}`;
-
     tokenModel.validate({ ...req.params, token: req.body.token })
-    .then(() => table.upsert('package', {
-      id,
-      provider,
-      user,
-      repo,
-      branch,
-      report,
-      dependencies: JSON.stringify(result.dependencies),
-      devDependencies: JSON.stringify(result.devDependencies),
-    }))
+    .then(() => packageModel.upsert({ ...req.params, ...req.body }))
     .then(entity => res.json(entity),
       error => res.send(error.toString()));
   });
