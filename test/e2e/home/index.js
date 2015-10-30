427b1905a4fe7f5a4e2478d356dda1835394dcd3
Junle <lijunle@gmail.com>
2015-10-31 01:46:52 +0800

    Update home page E2E test and view unit test.

--- test/e2e/home/index.js
+++ test/e2e/home/index.js
@@ -11,19 +11,19 @@ describe('/', () => {
     })
     .get('/')
     .expect(200)
-    .expect(/Depcheck Web Service/)
+    .expect(/Depcheck/)
     .expect(/Login/)
     .end(done));
 
   it('should render welcome and go to repo link if user login', done =>
     stub({
       session: {
-        login: '/github/tester',
+        login: '/e2e/tester',
       },
     })
     .get('/')
     .expect(200)
-    .expect(/Hello tester/)
-    .expect(/my repo/)
+    .expect(/e2e\/tester/)
+    .expect(/repository list/)
     .end(done));
 });
--- test/views/home.js
+++ test/views/home.js
@@ -4,20 +4,33 @@ import 'should';
 import view from '../utils/view';
 
 describe('view home', () => {
+  it('should render caption, introduction and logo', () => {
+    const query = view('home', {});
+
+    const caption = query('body > h1');
+    caption.text().should.equal('Depcheck');
+
+    const introduction = query('.row p');
+    introduction.text().should.containEql('Depcheck')
+      .and.containEql('unused dependencies');
+
+    const logo = query('body img');
+    logo.attr('src').should.containEql('logo');
+    logo.attr('alt').should.equal('Depcheck Logo');
+  });
+
   it('should render login URL when no login information', () => {
     const query = view('home', {});
 
-    const caption = query('body h1');
-    caption.text().should.equal('Depcheck Web Service');
-
-    const anchor = query('body a');
-    anchor.attr('href').should.equal('/login/github');
-    anchor.text().should.match(/^login/i);
+    const button = query('body a.btn');
+    button.attr('href').should.equal('/login/github');
+    button.text().should.containEql('Login').and.containEql('GitHub');
   });
 
   it('should render go to my repo link when login information', () => {
     const properties = {
       login: {
+        provider: 'views',
         user: 'tester',
       },
       url: {
@@ -31,6 +44,8 @@ describe('view home', () => {
     anchor.attr('href').should.equal(properties.url.repoList);
 
     const paragraph = query('body p');
-    paragraph.text().should.match(/hello tester/i);
+    paragraph.text().should.containEql('Logged in')
+      .and.containEql('views/tester')
+      .and.containEql('repository list');
   });
 });
