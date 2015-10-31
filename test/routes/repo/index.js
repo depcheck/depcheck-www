8f8970531b53c9a679a8c62a5ff686ed5e1293a7
Junle <lijunle@gmail.com>
2015-10-31 23:14:59 +0800

    Update the test correspondingly.

--- test/routes/repo/index.js
+++ test/routes/repo/index.js
@@ -12,9 +12,28 @@ describe('route repo index', () => {
     };
 
     const query = mockFunction([
-      { name: 'project-1' },
-      { name: 'project-2' },
-      { name: 'project-3' },
+      {
+        name: 'enable-1',
+        token: 'token-1',
+      },
+      {
+        name: 'enable-2',
+        token: 'token-2',
+      },
+      {
+        name: 'enable-3',
+        token: 'token-3',
+      },
+      {
+        name: 'disable-1',
+      },
+      {
+        name: 'disable-2',
+      },
+      {
+        name: 'project-3',
+        invalid: true,
+      },
     ]);
 
     const repo = route('repo/index', {
@@ -27,9 +46,20 @@ describe('route repo index', () => {
     });
 
     return repo.model({ params: info })
-    .then(model =>
-      model.should.have.properties(info)
-        .and.have.property('repos').have.length(3))
+    .then(model => {
+      model.should.have.properties(info);
+      model.should.have.property('enabled').have.length(3);
+      model.should.have.property('disabled').have.length(2);
+      model.should.have.property('invalid').have.length(1);
+
+      model.enabled[0].should.have.properties({
+        enabled: true,
+        token: undefined,
+      }).and.have.properties([
+        'repoUrl',
+        'badgeUrl',
+      ]);
+    })
     .then(() =>
       query.calls[0][0].should.have.properties(info));
   });
--- test/views/repo-list.js
+++ test/views/repo-list.js
@@ -6,26 +6,27 @@ import view from '../utils/view';
 const data = {
   provider: 'views',
   user: 'tester',
-  repos: [
+  enabled: [
     {
       name: 'project-1',
       description: 'description-1',
-      token: 'token-1',
       repoUrl: '/project/1',
-      requestTokenUrl: '/token/project/1',
+      enabled: true,
+      badgeUrl: '/project/1/master.svg',
     },
+  ],
+  disabled: [
     {
       name: 'project-2',
       description: 'description-2',
-      token: 'token-2',
       repoUrl: '/project/2',
-      requestTokenUrl: '/token/project/2',
     },
+  ],
+  invalid: [
     {
       name: 'project-3',
-      description: 'description-3',
       repoUrl: '/project/3',
-      requestTokenUrl: '/token/project/3',
+      invalid: true,
     },
   ],
 };
@@ -34,30 +35,45 @@ describe('view repo list', () => {
   it('should render repo list', () => {
     const query = view('repo-list', data);
 
-    query('h1').text().should.equal('Repo List');
-    query('p').text().should.match(/views\/tester/);
-    query('ul > li').length.should.equal(data.repos.length);
+    query('h1').text().should.equal('Repository');
+    query('h5 > mark').text().should.match(/views\/tester/);
+
+    query('h3').text().should.equal('Invalid Repositories');
+    query('h3 ~ p').text().should.containEql('removed from views')
+      .and.containEql('invalid');
+
+    query('ul > li').length.should.equal(3);
+    query('ul').eq(0).find('li').length.should.equal(1);
+    query('ul').eq(1).find('li').length.should.equal(1);
+    query('ul').eq(2).find('li').length.should.equal(1);
   });
 
-  it('should render repo token if present', () => {
+  it('should render repo badge if enabled', () => {
     const query = view('repo-list', data);
     const repoItem = query('li').eq(0);
 
-    repoItem.find('h4').text().should.equal('project-1');
+    repoItem.find('h4').text().trim().should.equal('project-1');
     repoItem.find('h4 > a').attr('href').should.equal('/project/1');
+    repoItem.find('h4 img').attr('src').should.equal('/project/1/master.svg');
     repoItem.find('p').text().should.equal('description-1');
-    repoItem.find('code').text().should.equal('token-1');
   });
 
-  it('should render enable token button if token not present', () => {
+  it('should not render repo badge if disabled', () => {
+    const query = view('repo-list', data);
+    const repoItem = query('li').eq(1);
+
+    repoItem.find('h4').text().should.equal('project-2');
+    repoItem.find('h4 > a').attr('href').should.equal('/project/2');
+    repoItem.find('h4 img').should.length(0);
+    repoItem.find('p').text().should.equal('description-2');
+  });
+
+  it('should not render repo description if invalid', () => {
     const query = view('repo-list', data);
     const repoItem = query('li').eq(2);
 
     repoItem.find('h4').text().should.equal('project-3');
-    repoItem.find('p').text().should.equal('description-3');
-    repoItem.find('code').should.have.length(0);
-    repoItem.find('form').attr('action').should.equal('/token/project/3');
-    repoItem.find('form > input').attr('type').should.equal('submit');
-    repoItem.find('form > input').val().should.equal('Enable');
+    repoItem.find('h4 > a').attr('href').should.equal('/project/3');
+    repoItem.find('p').text().should.equal('');
   });
 });
