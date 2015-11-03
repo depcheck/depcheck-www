56116c648ae183530df97199a54d0bbc4b41c4ff
Junle <lijunle@gmail.com>
2015-11-03 21:46:38 +0800

    Add the tutorial view.
    - It reads the tutorial content from Markdown doc.

---
+++ src/views/tutorial.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import markdown from 'marked';
+import Layout from './layout';
+
+export default React.createClass({
+  render() {
+    return (
+      <Layout>
+        <h1 className="text-center">Tutorial</h1>
+        <div className="row">
+          <div
+            className="col-md-8 col-md-offset-2"
+            dangerouslySetInnerHTML={{__html: markdown(this.props.content)}}
+          />
+        </div>
+      </Layout>
+    );
+  },
+});
