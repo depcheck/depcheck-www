bc9a26029ef06f109bacbf743d98484f3073c00a
Junle <lijunle@gmail.com>
2015-11-06 03:16:48 +0800

    Construct the URL builders and pass from index.
    - Pass the URL builders into routes as parameter.
    - Refine the generate routes logic to strip `.js` extension.

--- src/routes/generate.js
+++ src/routes/generate.js
@@ -1,15 +1,31 @@
 import file from 'file';
+import { set } from 'lodash';
+import { compile } from 'path-to-regexp';
 
-export default function generateRoutes() {
+const startIndex = __dirname.length + 1;
+
+function toRoute(dirPath, name) {
+  const prefix = dirPath.substring(startIndex);
+  const basename = name.slice(0, -3); // strip .js extension
+  return `${prefix}/${basename}`;
+}
+
+export function generateRoutes() {
   let routes = [];
-  const startIndex = __dirname.length + 1;
 
-  file.walkSync(__dirname, (dirPath, dirs, files) => {
+  file.walkSync(__dirname, (dirPath, dirs, files) =>
     routes = routes.concat(
       dirPath !== __dirname
-      ? files.map(name => `${dirPath}/${name}`.substring(startIndex))
-      : []);
-  });
+      ? files.map(name => toRoute(dirPath, name))
+      : []));
 
   return routes;
+}
+
+export function generateUrls(modules) {
+  return modules.reduce((object, { name, module }) => {
+    const path = name.replace('/', '.'); // convert name to object path
+    const route = compile(module.route);
+    return set(object, path, route);
+  }, {});
 }
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import config from './routes';
-import generateRoutes from './generate';
 import { logger } from '../services';
+import { generateRoutes, generateUrls } from './generate';
 
 function handleError(res) {
   return (error) => {
@@ -24,6 +24,7 @@ function createRouter() {
   const router = new express.Router();
   const routes = config.generate ? generateRoutes() : config.routes;
   const modules = routes.map(name => ({ name, module: require(`./${name}`) }));
+  const urls = generateUrls(modules);
 
   modules.forEach(({ name, module }) => {
     const get = register(router, module.route, name, 'get');
@@ -33,7 +34,7 @@ function createRouter() {
 
     if (module.post) {
       post(module.post.middlewares, (req, res) =>
-        module.post(req).then(result =>
+        module.post({ ...req, urls }).then(result =>
           // TODO leverage `res.format` to better content-negotiation
           req.accepts('html') === 'html'
           ? res.redirect(req.get('Referer'))
@@ -42,10 +43,10 @@ function createRouter() {
 
     if (module.redirect) {
       get(module.redirect.middlewares, (req, res) =>
-        module.redirect(req).then(url => res.redirect(url)));
+        module.redirect({ ...req, urls }).then(url => res.redirect(url)));
     } else if (module.view && module.model) {
       get(module.model.middlewares, (req, res) =>
-        module.model(req).then(model =>
+        module.model({ ...req, urls }).then(model =>
           res.type(module.type || 'html').render(module.view, model)));
     }
   });
