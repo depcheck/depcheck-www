ece722ed49ab7c3d3443218a2f84f81506a29311
Junle Li <lijunle@gmail.com>
2015-11-07 18:28:03 +0800

    Update and refine the related test cases.
    - Add test case to cover `loginModel.isLoggedIn` API.

--- test/models/login.js
+++ test/models/login.js
@@ -3,17 +3,19 @@
 import 'should';
 import model from '../utils/model';
 
+const session = {
+  login: {
+    provider: 'models',
+    user: 'tester',
+  },
+};
+
 describe('model login', () => {
   it('should pass validation when user match', () =>
     model('login').validate({
       provider: 'models',
       user: 'tester',
-      session: {
-        login: {
-          provider: 'models',
-          user: 'tester',
-        },
-      },
+      session,
     })
     .then(() => 'everything'.should.be.ok()));
 
@@ -21,12 +23,23 @@ describe('model login', () => {
     model('login').validate({
       provider: 'models',
       user: 'hacker',
-      session: {
-        login: {
-          provider: 'models',
-          user: 'tester',
-        },
-      },
+      session,
     })
     .catch(error => error.should.have.properties(['code', 'message'])));
+
+  it('should return true when logged user is the owner of repo', () =>
+    model('login').isLoggedIn({
+      provider: 'models',
+      user: 'tester',
+      session,
+    })
+    .then(result => result.should.equal(true)));
+
+  it('should return false when logged user is not the owner of repo', () =>
+    model('login').isLoggedIn({
+      provider: 'models',
+      user: 'hacker',
+      session,
+    })
+    .catch(result => result.should.equal(false)));
 });
--- test/routes/repo/report.js
+++ test/routes/repo/report.js
@@ -13,19 +13,16 @@ const info = {
 
 describe('route repo report', () => {
   it('should call and return repo basic information', () => {
-    const validate = mockFunction();
     const query = mockFunction([]);
-    const get = mockFunction('project-token');
-
     const repo = route('repo/report', {
       '../../models/login': {
-        validate,
+        isLoggedIn: mockFunction(true),
       },
       '../../models/report': {
         query,
       },
       '../../models/token': {
-        get,
+        get: mockFunction('project-token'),
       },
     });
 
@@ -37,28 +34,24 @@ describe('route repo report', () => {
   });
 
   it('should return the reports', () => {
-    const validate = mockFunction();
-    const get = mockFunction('project-token');
-    const query = mockFunction([
-      {
-        branch: 'branch-1',
-        report: 'report-1',
-      },
-      {
-        branch: 'branch-2',
-        report: '',
-      },
-    ]);
-
     const repo = route('repo/report', {
       '../../models/login': {
-        validate,
+        isLoggedIn: mockFunction(true),
       },
       '../../models/report': {
-        query,
+        query: mockFunction([
+          {
+            branch: 'branch-1',
+            report: 'report-1',
+          },
+          {
+            branch: 'branch-2',
+            report: '',
+          },
+        ]),
       },
       '../../models/token': {
-        get,
+        get: mockFunction('project-token'),
       },
     });
 
@@ -82,27 +75,67 @@ describe('route repo report', () => {
     });
   });
 
-  it('should set token properly when repo not enabled', () => {
-    const validate = mockFunction();
-    const query = mockFunction([]);
-    const get = mockFunction(() => Promise.reject('not enabled'));
-
+  it('should return the reports even if user not logged in', () => {
     const repo = route('repo/report', {
       '../../models/login': {
-        validate,
+        isLoggedIn: mockFunction(false),
       },
       '../../models/report': {
-        query,
+        query: mockFunction([
+          {
+            branch: 'branch-1',
+            report: 'report-1',
+          },
+        ]),
       },
       '../../models/token': {
-        get,
+        get: mockFunction('project-token'),
+      },
+    });
+
+    return repo.model({ urls, params: info })
+    .then(model => model.reports.should.have.length(1));
+  });
+
+  it('should set token properly when repo not enabled', () => {
+    const repo = route('repo/report', {
+      '../../models/login': {
+        isLoggedIn: mockFunction(true),
+      },
+      '../../models/report': {
+        query: mockFunction([]),
+      },
+      '../../models/token': {
+        get: mockFunction(() => Promise.reject('not enabled')),
       },
     });
 
     return repo.model({ urls, params: info })
     .then(model => model.should.have.properties({
+      isOwner: true,
       token: undefined,
       tokenUrl: '/token/routes/tester/project',
+    }));
+  });
+
+  it('should set isOwner and token properly when user not logged in', () => {
+    const repo = route('repo/report', {
+      '../../models/login': {
+        isLoggedIn: mockFunction(false),
+      },
+      '../../models/report': {
+        query: mockFunction([]),
+      },
+      '../../models/token': {
+        get: mockFunction(() => Promise.reject('not enabled')),
+      },
+    });
+
+    return repo.model({ urls, params: info })
+    .then(model => model.should.have.properties({
+      isOwner: false,
+      token: null,
+      tokenUrl: null,
     }));
   });
 });
