a3ec1c0b761ba43467f353a466009118198457a1
Junle Li <lijunle@gmail.com>
2015-11-24 00:10:26 +0800

    Move the login user logic to login model.

--- src/models/login.js
+++ src/models/login.js
@@ -1,10 +1,17 @@
 import response from './response';
 import { logger } from '../services';
+import { getProvider } from '../providers';
 
-export function set({ session, provider, user }) {
+function setUser({ session, provider, user }) {
   logger.debug(`[model:login] set login with provider [${provider}] and user [${user}].`);
   session.login = { provider, user };
-  return Promise.resolve();
+  return Promise.resolve(user);
+}
+
+export function loginUser({ session, provider, code }) {
+  logger.debug(`[model:login] login user with provider [${provider}] and code [${code}].`);
+  return getProvider(provider).getUser(code)
+    .then(user => setUser({ session, provider, user }));
 }
 
 export function getUser({ login } = null) {
